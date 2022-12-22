5f56845b6b347778919ded54dd41df9060f17f44
CODY DE LA TORRE <cdelatorre414@gmail.com>
2022-12-21 17:01:42 -0800

    fini lab 3

---
+++ README.md
@@ -0,0 +1,6 @@
+LAB - Class 3
+Project: Madlib
+Author: Cody De La Torre
+
+Tests
+How do you run tests? pytest
--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,7 +1,36 @@
-def read_template():
+print("Welcome to a MadLib game. I will ask for adjectives or nouns and you will type in whatever you want")
 
 
-def parse_template():
+def read_template(string):
+    with open(string, 'r') as reader:
+        return reader.read()
 
 
-def merge():
+def parse_template(template):
+    words = []
+    stripped_sent = ""
+
+    capturing = False
+    captured_words = ""
+
+    for char in template:
+        if capturing:
+            if char == "}":
+                words.append(captured_words)
+                stripped_sent += char
+                captured_words = ""
+                capturing = False
+            else:
+                captured_words += char
+        else:
+            stripped_sent += char
+            if char == "{":
+                capturing = True
+    return stripped_sent, tuple(words)
+
+
+def merge(string, tup):
+    txt = string.format(tup[0], tup[1], tup[2])
+    return txt
+
+
